353292d6b9b08dff27d673d1b816bf38196c63f2
jacquelineteufel <jac.teufel.21@lehre.mosbach.dhbw.de>
2022-05-11 08:17:30 +0200

    function calculateCurrency() zum berechnen der währung eingebaut

--- src/calculation.ts
+++ src/calculation.ts
@@ -4,16 +4,24 @@ import { updateRates } from './api';
 
 //Deklarierung
 export let billResult: number
+export let billResultCurrency: number
 export let billPerPersonResult: number
+export let billPerPersonResultCurrency :number 
+
+
 
 
 //Rechnungsfunktion
-export function calculate() {
-  updateRates();
+export function calculateTip() {
+  //updateRates();
   billResult = bill * (tip / 100) + bill
   billPerPersonResult = billResult / persons
 
 }
 export function calculateCurrency(){
-
+  updateRates();
+  billResultCurrency = billResult // * test
+  console.log("1Ergebnis" + billResultCurrency)
+  billPerPersonResultCurrency = billPerPersonResult // * test
+  console.log("2Ergebnis" + billPerPersonResultCurrency)
 }
